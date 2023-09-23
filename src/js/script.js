61f1e55f5d757665374f0ba70d27e7f3f3a589a4
bard0ne <broadnicegamerr@gmail.com>
2023-09-23 22:39:14 +0200

    update

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+//window.alert("WORK IN PROGRESS")
+
+function myfunction(){
+    let text = document.getElementById('demo').innerHTML;
+    document.getElementById('demo').innerHTML = text.replace('suck', 'are beautiful');
+}
+console.log('se leggi questo devi sucare')
