d1aedb7b9e2284e5caf57b6dba203baaf05a3752
bard0ne <broadnicegamerr@gmail.com>
2023-09-24 00:46:33 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,3 @@ function buttonpress(){
     let text = document.getElementById('demo').innerHTML;
     document.getElementById('demo').innerHTML = text.replace('suck', 'are beautiful');
 }
-console.log('se leggi questo devi sucare')
